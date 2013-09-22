05594d16c49423746b3f07c67e7ce10513673dc0
Cameron <jcmaci03@gmail.com>
2013-09-21 20:42:12 -0500

    inital radar model.  Add to project.

--- radar/radar/models.py
+++ radar/radar/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class TechItem(models.Model):
+	name = models.CharField(max_length=100)
+	projectUrl = models.URLField()
+	parent = models.CharField(max_length=100)  # alt names: category, type, ...
+	def __unicode__(self):
+		return self.name
+
+
+RADAR_OPTIONS = (
+	('A', 'Adopt'),
+	('H', 'Hold'),
+	('T', 'Trial'),
+	('R', 'Reject'),
+)
+class ItemReview(models.Model):
+	person = models.CharField(max_length=100)
+	techItem = models.ForeignKey('TechItem')
+	grade = models.IntegerField()
+	radar = models.CharField(max_length=1, choices=RADAR_OPTIONS)
+	notes = models.TextField(max_length=500)
+	project = models.CharField(max_length=100)
+	def __unicode__(self):
+		return "Review of " + self.techItem.name + " by " + self.person
+
+#################
+# Admin Classes #
+#################
+
+admin.site.register(ItemReview)
+admin.site.register(TechItem)
